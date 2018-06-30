ca6294d39aba1c638fdb9c195d4563927f9823af
sumit-j <Sumit.Joshi@nbcuni.com>
2018-06-30 18:39:54 +0530

    first master commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecepiesComponent } from './recepies/recepies.component';
-import { RecepiesListComponent } from './recepies/recepies-list/recepies-list.component';
-import { RecepiesDetailComponent } from './recepies/recepies-detail/recepies-detail.component';
-import { RecepiesItemComponent } from './recepies/recepies-list/recepies-item/recepies-item.component';
+import { RecepieComponent } from './recepie/recepie.component';
 import { ShopingListComponent } from './shoping-list/shoping-list.component';
-import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
+import { RecepieListComponent } from './recepie/recepie-list/recepie-list.component';
+import { RecepieDetaliComponent } from './recepie/recepie-detali/recepie-detali.component';
+import { RecepieItemComponent } from './recepie/recepie-list/recepie-item/recepie-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecepiesComponent,
-    RecepiesListComponent,
-    RecepiesDetailComponent,
-    RecepiesItemComponent,
+    RecepieComponent,
     ShopingListComponent,
-    ShopingEditComponent
+    RecepieListComponent,
+    RecepieDetaliComponent,
+    RecepieItemComponent,
+ 
   ],
   imports: [
     BrowserModule
---
+++ src/app/recepie/recepie-detali/recepie-detali.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie-detali',
+  templateUrl: './recepie-detali.component.html',
+  styleUrls: ['./recepie-detali.component.css']
+})
+export class RecepieDetaliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
